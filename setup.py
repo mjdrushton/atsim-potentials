bab21c9b5518d387fbc5e609da0994d499fe7788
Michael Rushton <m.j.d.rushton@gmail.com>
2018-05-01 19:41:35 +0100

    Updated setup.py to allow upload to pypi.

--- setup.py
+++ setup.py
@@ -30,5 +30,9 @@ setup(name="atsim.potentials",
     "atomic scale"],
   classifiers = [
     "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 2.7 :: 3.5 :: 3.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Scientific/Engineering"])
